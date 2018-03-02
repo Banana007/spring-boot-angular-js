91017a0bc47b47edd5b254cc0a3a755145923ee0
Evgenii Butkov <eugenefromrus@gmail.com>
2018-03-03 00:26:03 +1000

    Initial commit

---
+++ src/main/java/com/mycompany/springbootangularjs/SpringBootAngularJsApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.springbootangularjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularJsApplication.class, args);
+	}
+}
